26dc7bac6f014949f3802e0372c27066b7dc228d
Federico Di Pierro <nierro92@gmail.com>
2022-04-04 15:56:51 +0200

    new: added a cron based github action to automatically push new list of supported kernels.
    Output can be found for each platform in kernels/ subdir.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- probe_builder/__init__.py
+++ probe_builder/__init__.py
@@ -1,17 +1,11 @@
 import logging
-import os
-import sys
 import json
+import sys
 import click
 
 from probe_builder.kernel_crawler import crawl_kernels, DISTROS
-from . import kernel_crawler, disable_ipv6, git, docker
-from .builder import choose_builder, builder_image
-from .builder.distro import Distro
-from .context import Context, Workspace, Probe, DownloadConfig
 
 logger = logging.getLogger(__name__)
-
 
 def init_logging(debug):
     level = 'DEBUG' if debug else 'INFO'
@@ -22,110 +16,10 @@ def init_logging(debug):
     logger.addHandler(handler)
     logger.debug("DEBUG logging enabled")
 
-
-def get_probe(workspace, sysdig_dir, probe_name, probe_version):
-    workspace_dir = workspace.workspace
-
-    try:
-        probe_device_name, _ = probe_name.split('-', 1)
-    except ValueError:
-        probe_device_name = probe_name
-
-    if sysdig_dir is None:
-        sysdig_dir = os.path.join(workspace_dir, 'sysdig')
-        if probe_device_name == 'sysdigcloud':
-            repo = 'https://github.com/draios/agent-libs'
-            branch = 'agent/{}'.format(probe_version)
-        elif probe_device_name == 'sysdig':
-            repo = 'https://github.com/draios/sysdig'
-            branch = probe_version
-        else:
-            raise RuntimeError('Unsupported probe type {}'.format(probe_name))
-        git.clone(repo, branch, sysdig_dir)
-    else:
-        sysdig_dir = os.path.abspath(sysdig_dir)
-
-    if not os.path.exists(sysdig_dir):
-        click.echo('Driver source directory {} does not exist'.format(sysdig_dir), err=True)
-        sys.exit(1)
-
-    return Probe(sysdig_dir, probe_name, probe_version, probe_device_name)
-
-
-class CrawlDistro(object):
-
-    def __init__(self, distro, builder_distro, crawler_distro):
-        self.distro_obj = Distro(distro, builder_distro)
-        self.distro_builder = self.distro_obj.builder()
-        self.crawler_distro = crawler_distro
-
-    def get_kernels(self, workspace, _packages, download_config, filter):
-        return self.distro_builder.crawl(workspace, self.distro_obj, self.crawler_distro, download_config, filter)
-
-class LocalDistro(object):
-
-    def __init__(self, distro, builder_distro):
-        self.distro_obj = Distro(distro, builder_distro)
-        self.distro_builder = self.distro_obj.builder()
-
-    def get_kernels(self, _workspace, packages, _download_config, filter):
-        return self.distro_builder.batch_packages(packages)
-
-
-CLI_DISTROS = {
-    'AmazonLinux': CrawlDistro('amazonlinux', 'centos', 'AmazonLinux'),
-    'AmazonLinux2': CrawlDistro('amazonlinux2', 'centos', 'AmazonLinux2'),
-    'CentOS': CrawlDistro('centos', 'centos', 'CentOS'),
-    'Debian': CrawlDistro('debian', 'debian', 'Debian'),
-    'Fedora': CrawlDistro('fedora', 'centos', 'Fedora'),
-    'Flatcar': CrawlDistro('flatcar', 'flatcar', 'Flatcar'),
-    'Oracle6': CrawlDistro('oracle6', 'oracle', 'Oracle6'),
-    'Oracle7': CrawlDistro('oracle7', 'oracle', 'Oracle7'),
-    'Oracle8': CrawlDistro('oracle8', 'oracle', 'Oracle8'),
-    'Ubuntu': CrawlDistro('ubuntu', 'ubuntu', 'Ubuntu'),
-    'CustomCentOS': LocalDistro('custom-centos', 'centos'),
-    'CustomDebian': LocalDistro('custom-debian', 'debian'),
-    'CustomUbuntu': LocalDistro('custom-ubuntu', 'ubuntu'),
-    'CustomFlatcar': LocalDistro('custom-flatcar', 'flatcar'),
-}
-
-
 @click.group()
 @click.option('--debug/--no-debug')
 def cli(debug):
     init_logging(debug)
-
-
-@click.command()
-@click.option('-b', '--builder-image-prefix', default='')
-@click.option('-d', '--download-concurrency', type=click.INT, default=1)
-@click.option('-k', '--kernel-type', type=click.Choice(sorted(CLI_DISTROS.keys())))
-@click.option('-f', '--filter', default='')
-@click.option('-p', '--probe-name')
-@click.option('-r', '--retries', type=click.INT, default=1)
-@click.option('-s', '--source-dir')
-@click.option('-t', '--download-timeout', type=click.FLOAT)
-@click.option('-v', '--probe-version')
-@click.argument('package', nargs=-1)
-def build(builder_image_prefix,
-          download_concurrency, kernel_type, filter, probe_name, retries,
-          source_dir, download_timeout, probe_version, package):
-    workspace_dir = os.getcwd()
-    builder_source = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    workspace = Workspace(docker.is_privileged(), docker.get_mount_mapping(), workspace_dir, builder_source, builder_image_prefix)
-    probe = get_probe(workspace, source_dir, probe_name, probe_version)
-    distro_obj = CLI_DISTROS[kernel_type]
-
-    distro_builder = distro_obj.distro_builder
-    distro = distro_obj.distro_obj
-    download_config = DownloadConfig(download_concurrency, download_timeout, retries, None)
-
-    kernels = distro_obj.get_kernels(workspace, package, download_config, filter)
-    kernel_dirs = distro_builder.unpack_kernels(workspace, distro.distro, kernels)
-    for release, target in kernel_dirs:
-        distro_builder.build_kernel(workspace, probe, distro.builder_distro, release, target)
-
 
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -151,8 +45,6 @@ def crawl(distro, version='', arch='', json_fmt=False):
         json_object = json.dumps(kernels, indent=4, cls=SetEncoder)
         print(json_object)
 
-
-cli.add_command(build, 'build')
 cli.add_command(crawl, 'crawl')
 
 if __name__ == '__main__':
