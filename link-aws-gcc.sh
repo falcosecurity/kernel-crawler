6344557d0588fabb6fb2787585a348b6b184b951
Federico Di Pierro <nierro92@gmail.com>
2022-04-05 09:46:20 +0200

    chore(kernel_crawler): dropped unused files.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- link-aws-gcc.sh
+++
@@ -1,6 +0,0 @@
-executables=(addr2line ar as c++filt c89 c99 cc cpp dwp elfedit gcc gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool gprof ld ld.bfd ld.gold lto-dump nm objcopy objdump ranlib readelf size strings strip)
-
-for i in "${executables[@]}"
-do
-	ln -s /usr/bin/$i /usr/bin/gcc10-$i
-done
--- setup.py
+++ setup.py
@@ -2,22 +2,16 @@
 
 from distutils.core import setup
 
-setup(name='probe_builder',
-      version='1.0',
-      description='Sysdig probe builder',
+setup(name='kernel_crawler',
+      version='1.0.0',
+      description='Falcosecurity kernel crawler',
       author='Grzegorz Nosek',
       author_email='grzegorz.nosek@sysdig.com',
-      url='https://www.sysdig.com/',
-      packages=['probe_builder'],
+      url='https://falco.org/',
+      packages=['kernel_crawler'],
       install_requires=[
           'click',
           'requests',
           'lxml',
       ],
-      entry_points={
-          'console_scripts': [
-              'probe_builder = probe_builder:cli',
-              'artifactory_download = probe_builder.artifactory_download:cli',
-          ],
-      },
-      )
+)
