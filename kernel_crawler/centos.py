cab7f3f71c9cf3de3032b12af192d58fefd7547b
Logan Bond <lbond@secureworks.com>
2022-07-18 14:58:49 -0500

    switching to vault.centos.org and adding archive.kernel.org
    Signed-off-by: Logan Bond <lbond@secureworks.com>

--- kernel_crawler/centos.py
+++ kernel_crawler/centos.py
@@ -19,9 +19,12 @@ class CentosMirror(repo.Distro):
             rpm.RpmMirror('http://mirror.centos.org/centos/', 'updates/' + arch + '/', v7_only),
             # CentOS 8 reached end-of-life at the end of 2021, so no point looking for it
             # rpm.RpmMirror('http://mirror.centos.org/centos/', 'BaseOS/' + arch + '/os/', v8_only),
-            rpm.RpmMirror('http://linuxsoft.cern.ch/centos-vault/', 'os/' + arch + '/', v6_or_v7),
-            rpm.RpmMirror('http://linuxsoft.cern.ch/centos-vault/', 'updates/' + arch + '/', v6_or_v7),
-            rpm.RpmMirror('http://linuxsoft.cern.ch/centos-vault/', 'BaseOS/' + arch + '/os/', v8_only),
+            rpm.RpmMirror('http://vault.centos.org/centos/', 'os/' + arch + '/', v6_or_v7),
+            rpm.RpmMirror('http://vault.centos.org/centos/', 'updates/' + arch + '/', v6_or_v7),
+            rpm.RpmMirror('http://vault.centos.org/centos/', 'BaseOS/' + arch + '/os/', v8_only),
+            rpm.RpmMirror('http://archive.kernel.org/centos/', 'os/' + arch + '/', v6_or_v7),
+            rpm.RpmMirror('http://archive.kernel.org/centos/', 'updates/' + arch + '/', v6_or_v7),
+            rpm.RpmMirror('http://archive.kernel.org/centos/', 'BaseOS/' + arch + '/os/', v8_only),
         ]
         super(CentosMirror, self).__init__(mirrors, arch)
 
