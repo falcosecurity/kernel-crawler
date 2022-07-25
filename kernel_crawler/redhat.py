9d9f16ae56555b7e70058bf398c2eb291d74f6cc
John Brydon <jbrydon@secureworks.com>
2022-07-25 15:13:37 +0100

    moved image loop to 'crawl_kernels'
    Signed-off-by: John Brydon <jbrydon@secureworks.com>

--- kernel_crawler/redhat.py
+++ kernel_crawler/redhat.py
@@ -3,18 +3,17 @@ from .container import Container
 import re
 
 class RedhatContainer(repo.ContainerDistro):
-    def __init__(self, images):
-        super(RedhatContainer, self).__init__(images)
+    def __init__(self, image):
+        super(RedhatContainer, self).__init__(image)
 
     def get_kernel_versions(self):
         kernels = {}
-        for image in self.images:
-            c = Container(image)
-            cmd_out = c.run_cmd("repoquery --show-duplicates kernel-devel")
-            for log_line in cmd_out:
-                m = re.search("(?<=kernel-devel-0:).*", log_line);
-                if m:
-                    kernels[m.group(0)] = []
+        c = Container(self.image)
+        cmd_out = c.run_cmd("repoquery --show-duplicates kernel-devel")
+        for log_line in cmd_out:
+            m = re.search("(?<=kernel-devel-0:).*", log_line);
+            if m:
+                kernels[m.group(0)] = []
         return kernels
 
     def to_driverkit_config(self, release, deps):
