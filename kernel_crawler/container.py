3f5b011510b7eebcd57665baf0bdc69a29fce474
John Brydon <jbrydon@secureworks.com>
2022-07-25 17:05:35 +0100

    fixed output of container command when buffered
    Signed-off-by: John Brydon <jbrydon@secureworks.com>

--- kernel_crawler/container.py
+++ kernel_crawler/container.py
@@ -14,10 +14,12 @@ class Container():
         client = docker.from_env()
         container = client.containers.run(self.image, cmd, detach=True)
         logs = container.attach(stdout=True, stderr=True, stream=True, logs=True)
-        cmd_output = []
+        # Depending on the command, the output could be buffered so first amalgamate
+        # into one byte stream so that the outut can be processed correctly.
         with click.progressbar(logs, label='[' + self.image + '] Running command \'' + cmd + '\'', item_show_func=decoded_str) as logs:
+            output = b''
             for line in logs:
-                decoded_line = line.decode(encoding)
-                sp = list(filter(None, decoded_line.split("\n")))
-                cmd_output.extend(sp)
+                output += line
+        decoded_line = output.decode(encoding)
+        cmd_output = list(filter(None, decoded_line.split("\n")))
         return cmd_output
