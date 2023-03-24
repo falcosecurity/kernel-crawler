6ae769ffd992710288255ca81e30531be8663a08
Federico Di Pierro <nierro92@gmail.com>
2023-03-24 12:03:52 +0100

    update(kernel_crawler): updated oracle to support OracleLinux9.
    Moreover, unified OracleLinux{6,7,8,9} under single `OracleLinux`.
    Finally, renamed output target to `ol`, that is the `ID` got from `/etc/os-release` on OracleLinux.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- kernel_crawler/oracle.py
+++ kernel_crawler/oracle.py
@@ -1,65 +1,41 @@
 from . import repo
 from . import rpm
 
+
 class OracleRepository(rpm.RpmRepository):
     @classmethod
     def kernel_package_query(cls):
         return '''(name IN ('kernel', 'kernel-devel', 'kernel-uek', 'kernel-uek-devel') AND arch = 'x86_64')'''
 
 
-class Oracle6Mirror(repo.Distro):
+class OracleMirror(repo.Distro):
     def repos(self):
         return [
+            # Oracle6
             'http://yum.oracle.com/repo/OracleLinux/OL6/latest/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL6/MODRHCK/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL6/UEKR4/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL6/UEKR3/latest/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL6/UEK/latest/' + self.arch + '/',
-        ]
-
-    def __init__(self, arch):
-        super(Oracle6Mirror, self).__init__([], arch)
-
-    def list_repos(self):
-        return [OracleRepository(url) for url in self.repos()]
-
-    def to_driverkit_config(self, release, deps):
-        for dep in deps:
-            if dep.find("devel") != -1:
-                return repo.DriverKitConfig(release, "oracle6", dep)
-
-class Oracle7Mirror(repo.Distro):
-    def repos(self):
-        return [
+            # Oracle7
             'http://yum.oracle.com/repo/OracleLinux/OL7/latest/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL7/MODRHCK/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL7/UEKR6/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL7/UEKR5/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL7/UEKR3/' + self.arch + '/',
-        ]
-
-    def __init__(self, arch):
-        super(Oracle7Mirror, self).__init__([], arch)
-
-    def list_repos(self):
-        return [OracleRepository(url) for url in self.repos()]
-
-    def to_driverkit_config(self, release, deps):
-        for dep in deps:
-            if dep.find("devel") != -1:
-                return repo.DriverKitConfig(release, "oracle7", dep)
-
-
-class Oracle8Mirror(repo.Distro):
-    def repos(self):
-        return [
+            # Oracle8
             'http://yum.oracle.com/repo/OracleLinux/OL8/baseos/latest/' + self.arch + '/',
             'http://yum.oracle.com/repo/OracleLinux/OL8/UEKR6/' + self.arch + '/',
+            'http://yum.oracle.com/repo/OracleLinux/OL8/appstream/' + self.arch + '/',
+            # Oracle9
+            'http://yum.oracle.com/repo/OracleLinux/OL9/baseos/latest/' + self.arch + '/',
+            'http://yum.oracle.com/repo/OracleLinux/OL9/UEKR7/' + self.arch + '/',
+            'http://yum.oracle.com/repo/OracleLinux/OL9/appstream/' + self.arch + '/',
         ]
 
     def __init__(self, arch):
-        super(Oracle8Mirror, self).__init__([], arch)
+        super(OracleMirror, self).__init__([], arch)
 
     def list_repos(self):
         return [OracleRepository(url) for url in self.repos()]
@@ -67,4 +43,4 @@ class Oracle8Mirror(repo.Distro):
     def to_driverkit_config(self, release, deps):
         for dep in deps:
             if dep.find("devel") != -1:
-                return repo.DriverKitConfig(release, "oracle8", dep)
+                return repo.DriverKitConfig(release, "ol", dep)
