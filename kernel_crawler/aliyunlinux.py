3eadf99cc1bed10500929edc836ac6957ac03818
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2023-03-26 12:59:25 +0200

    Alibaba Cloud Linux(AliyunLinux) 2 and 3 kernel crawl
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>

---
+++ kernel_crawler/aliyunlinux.py
@@ -0,0 +1,44 @@
+from . import repo
+from . import rpm
+
+def v2_only(ver):
+    return ver.startswith('2')
+
+def v3_only(ver):
+    return ver.startswith('3')
+
+class AliyunLinux2Mirror(repo.Distro):
+    def __init__(self, arch):
+        mirrors = [
+            # AliyunLinux 2
+            # Mirror list on cloud-init config example:
+            # https://www.alibabacloud.com/help/en/elastic-compute-service/latest/use-alibaba-cloud-linux-2-images-in-an-on-premises-environment
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'os/' + arch + '/', v2_only),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'updates/' + arch + '/', v2_only),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/' + arch + '/', v2_only),
+
+        ]
+        super(AliyunLinux2Mirror, self).__init__(mirrors, arch)
+
+    def to_driverkit_config(self, release, deps):
+        for dep in deps:
+            if dep.find("devel") != -1:
+                return repo.DriverKitConfig(release, "aliyunlinux2", dep)
+
+class AliyunLinux3Mirror(repo.Distro):
+    def __init__(self, arch):
+        mirrors = [
+            # AliyunLinux 3
+            # Mirror list on cloud-init config example:
+            # https://www.alibabacloud.com/help/en/elastic-compute-service/latest/use-alibaba-cloud-linux-3-images-in-an-on-premises-environment
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'os/' + arch + '/', v3_only),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'updates/' + arch + '/', v3_only),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/' + arch + '/', v3_only),
+
+        ]
+        super(AliyunLinux3Mirror, self).__init__(mirrors, arch)
+
+    def to_driverkit_config(self, release, deps):
+        for dep in deps:
+            if dep.find("devel") != -1:
+                return repo.DriverKitConfig(release, "aliyunlinux3", dep)
