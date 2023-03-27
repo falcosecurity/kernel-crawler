de3e7def846db953f4c1c1aff879d4d6ebed1f67
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2023-03-27 13:14:02 +0200

    merged aliyun linux 2 and 3 mirrors into a single mapping
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>

--- kernel_crawler/aliyunlinux.py
+++ kernel_crawler/aliyunlinux.py
@@ -7,7 +7,7 @@ def v2_only(ver):
 def v3_only(ver):
     return ver.startswith('3')
 
-class AliyunLinux2Mirror(repo.Distro):
+class AliyunLinuxMirror(repo.Distro):
     def __init__(self, arch):
         mirrors = [
             # AliyunLinux 2
@@ -17,17 +17,6 @@ class AliyunLinux2Mirror(repo.Distro):
             rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'updates/' + arch + '/', v2_only),
             rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/' + arch + '/', v2_only),
 
-        ]
-        super(AliyunLinux2Mirror, self).__init__(mirrors, arch)
-
-    def to_driverkit_config(self, release, deps):
-        for dep in deps:
-            if dep.find("devel") != -1:
-                return repo.DriverKitConfig(release, "alinux", dep)
-
-class AliyunLinux3Mirror(repo.Distro):
-    def __init__(self, arch):
-        mirrors = [
             # AliyunLinux 3
             # Mirror list on cloud-init config example:
             # https://www.alibabacloud.com/help/en/elastic-compute-service/latest/use-alibaba-cloud-linux-3-images-in-an-on-premises-environment
@@ -36,7 +25,7 @@ class AliyunLinux3Mirror(repo.Distro):
             rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/' + arch + '/', v3_only),
 
         ]
-        super(AliyunLinux3Mirror, self).__init__(mirrors, arch)
+        super(AliyunLinuxMirror, self).__init__(mirrors, arch)
 
     def to_driverkit_config(self, release, deps):
         for dep in deps:
